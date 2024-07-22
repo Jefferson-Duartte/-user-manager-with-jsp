6ae26973468ebcf92f5df6842584fc17413198a6
Jefferson Duarte <duartejefferson123@gmail.com>
2024-07-22 12:01:30 -0300

    feat: getting url and checking if it is not null

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -1,6 +1,6 @@
 package servlets;
 
-import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.RequestDispatcher;   
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,7 +11,7 @@ import model.Login;
 import java.io.IOException;
 
 
-@WebServlet("/ServletLogin")
+@WebServlet(urlPatterns = {"/ServletLogin", "/main/ServletLogin"})
 public class ServletLogin extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -21,6 +21,7 @@ public class ServletLogin extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String login = request.getParameter("login");
         String senha = request.getParameter("senha");
+        String url = request.getParameter("url");
 
         if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
             Login userLogin = new Login();
@@ -28,8 +29,13 @@ public class ServletLogin extends HttpServlet {
             userLogin.setSenha(senha);
 
             if (userLogin.getLogin().equalsIgnoreCase("admin") && userLogin.getSenha().equalsIgnoreCase("admin")) {
-                request.setAttribute("usuario", userLogin.getLogin());
-                RequestDispatcher redirect = request.getRequestDispatcher("main/main.jsp");
+
+                request.getSession().setAttribute("usuario", userLogin.getLogin());
+
+                if(url == null || url.equals("null")){
+                    url = "/main/main.jsp";
+                }
+                RequestDispatcher redirect = request.getRequestDispatcher(url);
                 redirect.forward(request, response);
             } else {
                 request.setAttribute("msg", "Informe o login e senha corretamente!");
