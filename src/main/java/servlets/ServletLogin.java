88baaa31dcebfe33490db45d94baec19d91601fb
Jefferson Duarte <duartejefferson123@gmail.com>
2024-07-30 15:00:29 -0300

    feat: implement method doGet and fixes some attributes

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -11,32 +11,42 @@ import model.Login;
 
 import java.io.IOException;
 
-
 @WebServlet(urlPatterns = {"/ServletLogin", "/main/ServletLogin"})
 public class ServletLogin extends HttpServlet {
 
     private DAOLoginRepository daoLoginRepositoty = new DAOLoginRepository();
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String action = request.getParameter("Logout");
+        if (action != null && action.equalsIgnoreCase("logout")) {
+            request.getSession().invalidate();
+            RequestDispatcher redirect = request.getRequestDispatcher("index.jsp");
+            redirect.forward(request, response);
+        } else {
+            doPost(request, response);
+        }
 
     }
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String login = request.getParameter("login");
-        String senha = request.getParameter("password");
+        String password = request.getParameter("password");
         String url = request.getParameter("url");
 
         try {
-            if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+            if (login != null && !login.isEmpty() && password != null && !password.isEmpty()) {
                 Login userLogin = new Login();
                 userLogin.setLogin(login);
-                userLogin.setPassword(senha);
+                userLogin.setPassword(password);
 
                 if (daoLoginRepositoty.validateAuthentication(userLogin)) {
 
-                    request.getSession().setAttribute("usuario", userLogin.getLogin());
+                    request.getSession().setAttribute("user", userLogin.getLogin());
 
                     if (url == null || url.equals("null")) {
                         url = "/main/main.jsp";
@@ -48,21 +58,15 @@ public class ServletLogin extends HttpServlet {
                     RequestDispatcher redirect = request.getRequestDispatcher("/index.jsp");
                     redirect.forward(request, response);
                 }
-
             } else {
-                request.setAttribute("msg", "Informe o login e senha corretamente!");
                 RequestDispatcher redirect = request.getRequestDispatcher("/index.jsp");
                 redirect.forward(request, response);
-
             }
-
         } catch (Exception e) {
             e.printStackTrace();
-            request.setAttribute("msg", e.getMessage());
-            RequestDispatcher redirect = request.getRequestDispatcher("error.jsp");
+            request.setAttribute("msg", "Ocorreu um erro: " + e.getMessage());
+            RequestDispatcher redirect = request.getRequestDispatcher("/error.jsp");
             redirect.forward(request, response);
         }
-
     }
-
 }
