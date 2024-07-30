a1a3804d3b8a9216e652bb9f65e7a48535d349d9
Jefferson Duarte <duartejefferson123@gmail.com>
2024-07-30 14:48:19 -0300

    feat: add new attributes and method to validate if the login already exists

--- src/main/java/model/Login.java
+++ src/main/java/model/Login.java
@@ -4,8 +4,45 @@ import java.io.Serializable;
 
 public class Login implements Serializable {
 
+    private Long id;
+    private String name;
+    private String email;
     private String login;
     private String password;
+
+    public boolean isNew() {
+        if (this.id == null) {
+            return true;
+        } else if (this.id != null && this.id > 0) {
+            return false;
+        }
+
+        return id == null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getLogin() {
         return login;
